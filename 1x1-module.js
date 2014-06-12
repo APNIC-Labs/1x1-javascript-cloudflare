5e8ac6b49f458c06079703e8d880ca62b8bc779f
George Michaelson <ggm@ggmwork.local>
2014-06-12 13:55:26 +1000

    add api key and bump version

--- 1x1-module.js
+++ 1x1-module.js
@@ -2,8 +2,8 @@
 // variable specific to a domain/account
  
 CloudFlare.define(
-    '<appKey>',
-    ['<appKey>/config'],
+    'cloudflare1x1measurement',
+    ['cloudflare1x1measurement/config'],
     function(config) {
  
         // URL for the JS resource. this fetches a minified blob with .vcf100 as the version string in the URLs of the 1x1.png fetches, includes WebRTC and uses cookies
